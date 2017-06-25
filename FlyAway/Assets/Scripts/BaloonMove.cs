1c10fe901aeeaf690466e8ec12bef5dec3ea7f33
Ergün Atik <ergunatik@hotmail.com>
2017-06-25 13:47:45 +0300

    no message

---
+++ FlyAway/Assets/Scripts/BaloonMove.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaloonMove : MonoBehaviour {
+
+	Rigidbody2D baloon;
+	float speed = 1f;
+	float x;
+	private Vector2 endPos;
+	private Vector2 startpos;
+	private Quaternion startRot;
+	private Quaternion endRot;
+	private float degree = 0;
+
+	// Use this for initialization
+	void Start () {
+		baloon = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		x = Input.GetAxis ("Horizontal");
+
+		Quaternion startRot = transform.rotation;
+
+		Vector2 startpos = transform.position;
+		Vector2 endPos = startpos + new Vector2 (x,0);
+
+		var localVel = endPos.x - startpos.x;
+
+		if (localVel > -1 && localVel < 1) {
+			degree = 0;
+		}
+		else if (localVel == -1) {
+			degree = 30;
+				}
+		else if (localVel == 1 ){
+			degree = -30;
+		}
+			
+
+		Quaternion endRot = Quaternion.Euler (0, 0, degree);
+		transform.position =  Vector2.Lerp (startpos, endPos, speed*Time.deltaTime);
+		transform.rotation = Quaternion.Lerp (startRot,endRot , 3f*Time.deltaTime);
+
+
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			baloon.velocity = new Vector2 (0, 3);
+		}
+	}
+}
---
+++ FlyAway/Assets/Scripts/ChellangeControl.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChellangeControl : MonoBehaviour {
+
+	public float scrollSpeed = 2.0f;
+	public GameObject[] challenges;
+	public GameObject[] chArrayleft;
+	public GameObject[] chArrayright;
+	public float frequency = 0.01f;
+	float counter= 0.0f;
+	public Transform challengeSpawnPoint;
+	public Transform challengeSpawnPoint2;
+	public Transform currentChallengeTransform;
+
+	// Use this for initialization
+	void Start () {
+		//GenerateRandomChallenge ();
+		chArrayleft = new GameObject[challenges.Length];
+		chArrayright = new GameObject[challenges.Length];
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//Generating
+			if (counter <= 0.0f) {
+				GenerateRandomChallenge ();
+			} else {
+				counter -= Time.deltaTime * frequency;
+			}
+
+
+		//Scrolling
+		GameObject currentChild;
+		for (int i = 0; i < transform.childCount; i++) {
+			currentChild = transform.GetChild (i).gameObject;
+			ScrollChallenge (currentChild);
+			currentChallengeTransform = currentChild.transform;
+			//Debug.Log (currentChallengeTransform.position);
+			if (currentChild.transform.position.y <= -7f) {
+				Destroy (currentChild);
+			}
+		}
+	
+
+	}
+	void ScrollChallenge (GameObject currentChallenge){
+		currentChallenge.transform.position -= Vector3.up * (scrollSpeed * Time.deltaTime);
+	}
+	void GenerateRandomChallenge(){
+		
+		int arrayCounter1 = 0;
+		int arrayCounter2 = 0;
+		int platformRange1 = Random.Range (0, 4);
+		int platformRange2 = 3 - platformRange1;
+		Debug.Log ("platform 1= " + platformRange1);
+		Debug.Log ("platform 2= "+ platformRange2);
+
+		foreach (GameObject challenge in challenges) {
+			string chName = challenge.name;
+			if (challenge.tag == platformRange1.ToString() && chName.Contains("left")){
+				chArrayleft [arrayCounter1] = challenge;
+				++arrayCounter1;
+			}
+		}
+		foreach (GameObject challenge in challenges) {
+			string chName = challenge.name;
+			if (challenge.tag == platformRange2.ToString() && chName.Contains("right")){
+				chArrayright [arrayCounter2] = challenge;
+				++arrayCounter2;
+			}
+		}
+
+		//var challanges = GameObject.FindWithTag ("1");
+		if (platformRange1 != 0) {
+			GameObject newChallenge1 = Instantiate (chArrayleft [Random.Range (0, arrayCounter1)], challengeSpawnPoint.position, Quaternion.identity) as GameObject;
+			newChallenge1.transform.parent = transform;
+		}
+		if (platformRange2 != 0) {
+			GameObject newChallenge2 = Instantiate (chArrayright [Random.Range (0, arrayCounter2)], challengeSpawnPoint2.position, Quaternion.identity) as GameObject;
+			newChallenge2.transform.parent = transform;
+		}
+		counter = 1.0f;
+	}
+
+}
